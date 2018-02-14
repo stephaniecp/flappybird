42d32ca48f7de874c0081f455e5c6ebeedf77482
Steph CP <stephers.cp@gmail.com>
2018-02-14 13:40:10 -0800

    first push

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flappybird
+
+#About
+In this repository, I am fallowing a tutorial to recreate the game *Flappy bird* using Phaser, an open source HTML5 game framework.
+
+To see the tutorial that I am using: http://www.lessmilk.com/tutorial/flappy-bird-phaser-1
---
+++ empty/main.js
@@ -0,0 +1,123 @@
+// Create our 'main' state that will contain the game
+var mainState = {
+  preload: function() {
+    // Load the bird sprite
+    game.load.image('bird', 'assets/bird.png');
+
+    game.load.image('pipe', 'assets/pipe.png');
+  },
+
+  create: function() {
+    // Change the background color of the game to blue
+    game.stage.backgroundColor = '#71c5cf';
+
+    // Set the physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    // Display the bird at the position x=100 and y=245
+    this.bird = game.add.sprite(100, 245, 'bird');
+
+    // Add physics to the bird
+    // Needed for: movements, gravity, collisions, etc.
+    game.physics.arcade.enable(this.bird);
+
+    // Add gravity to the bird to make it fall
+    this.bird.body.gravity.y = 1000;
+
+    // Call the 'jump' function when the spacekey is hit
+    var spaceKey = game.input.keyboard.addKey(
+        Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.jump, this);
+
+    // Create an empty group
+    this.pipes = game.add.group();
+
+    //To actually add pipes in our game we need to call the addRowOfPipes() function as timer every 1.5 seconds
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+    //to display the score in the top left
+    this.score = 0;
+    this.labelScore = game.add.text(20, 20, "0",
+        { font: "30px Arial", fill: "#ffffff" });
+  },
+
+
+  update: function() {
+    // If the bird is out of the screen (too high or too low)
+    // Call the 'restartGame' function
+    if (this.bird.y < 0 || this.bird.y > 490)
+      this.restartGame();
+
+    // to call restartGame() each time the bird collides with a pipe from the pipes group
+    game.physics.arcade.overlap(
+        this.bird, this.pipes, this.restartGame, null, this);
+  },
+
+  // Make the bird jump
+  jump: function() {
+    // Add a vertical velocity to the bird
+    this.bird.body.velocity.y = -350;
+  },
+
+  // Restart the game
+  restartGame: function() {
+    // Start the 'main' state, which restarts the game
+    game.state.start('main');
+  },
+
+
+  //new function to add a pipe in the game
+  addOnePipe: function(x, y) {
+    // Create a pipe at the position x and y
+    var pipe = game.add.sprite(x, y, 'pipe');
+
+    // Add the pipe to our previously created group
+    this.pipes.add(pipe);
+
+    // Enable physics on the pipe
+    game.physics.arcade.enable(pipe);
+
+    // Add velocity to the pipe to make it move left
+    pipe.body.velocity.x = -200;
+
+    // Automatically kill the pipe when it's no longer visible
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+  },
+
+  //to display 6 pipes in a row with a hole somewhere in the middle
+  addRowOfPipes: function() {
+    // Randomly pick a number between 1 and 5
+    // This will be the hole position
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    // Add the 6 pipes
+    // With one big hole at position 'hole' and 'hole + 1'
+    for (var i = 0; i < 8; i++) {
+      if (i != hole && i != hole + 1) {
+        this.addOnePipe(400, i * 60 + 10);
+      }
+    }
+
+    // to increase the score by 1 each time new pipes are created
+    this.score += 1;
+    this.labelScore.text = this.score;
+
+  },
+
+}; //end mainState
+
+// Initialize Phaser, and create a 400px by 490px game
+var game = new Phaser.Game(400, 490);
+
+// Add the 'mainState' and call it 'main'
+game.state.add('main', mainState);
+
+// Start the state to actually start the game
+game.state.start('main');
+
+// //to find if checkbox CheckBoxInvincible is checked
+// function invinsibleState() {
+//   var x = document.getElementById("checkBoxInvincible").checked;
+//   document.getElementById("demo").innerHTML = x;
+// }
